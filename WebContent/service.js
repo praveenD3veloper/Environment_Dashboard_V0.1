75a643e3b726aab258e1597cf066b4f34c4ae905
PraveenKumar1411 <PraveenKumar1411@github.com>
2018-07-31 22:30:19 +0530

    removing request obj dependency

--- WebContent/service.js
+++ WebContent/service.js
@@ -16,15 +16,16 @@ function initialLoad(){
 		console.log("Browser doesn't support Ajax");
 		window.alert("No Ajax support for the browser..!")
 	}
-	req.onreadystatechange=getResponse;
+	req.onreadystatechange=getAllesponse;
 	req.open('GET',"./fetchresponse?q=all",true);
 	req.send();
 }
 
-function getResponse(){
-	if(req.status==200 && req.readyState==4){
-		console.log("Servlet is ready to serve.");
-		console.log("response"+req.responseText.toString());
+//function to get all response 
+function getAllResponse(req){
+	if (req.status==200 && req.readyState==4){
+		console.log("Servlet is ready to server request...!");
+		console.log("Response from servlet for object "+req+req.responseText.toString);
 		var json=JSON.parse(req.responseText);
 		console.log(json);
 		var x;
@@ -44,6 +45,31 @@ function getResponse(){
 		}
 			}
 	}
+}
+
+//function to get response from requseted component
+function getResponse(){
+	if(req.status==200 && req.readyState==4){
+		console.log("Servlet is ready to serve request...!");
+		console.log("response"+req.responseText.toString());
+		var json=JSON.parse(req.responseText);
+		console.log(json);
+		var x;
+		for(x in json){
+			console.log("took the key: "+x);
+			console.log(json[x]);
+			if (json[x]=="R"){
+				console.log("coloring "+x+"as"+json[x]);
+				document.getElementById(x).style.backgroundColor="red";
+			}if (json[x]=="G"){
+				console.log("coloring "+x+"as"+json[x]);
+				document.getElementById(x).style.backgroundColor="green";
+		}
+			if (json[x]=="A"){
+				console.log("coloring "+x+"as"+json[x]);
+				document.getElementById(x).style.backgroundColor="#ff9933";
+		}
+			}
 }
 
 function render(){
